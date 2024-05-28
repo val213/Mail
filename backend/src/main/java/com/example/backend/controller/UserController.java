6d2e5f196e486de445c7a209a8f23422eda8e04f
smallcjy <2628035541@qq.com>
2024-05-29 00:08:32 +0800

    实现用户登录，修改信息功能接口

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -41,5 +41,7 @@ public class UserController
         return result;
     }
 
+    //
+
 }
 
--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -2,6 +2,10 @@ package com.example.backend.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.backend.entity.User;
+import com.example.backend.result.Result;
 
 public interface UserService extends IService<User>
-{}
+{
+    //修改用户信息，更新用户信息
+    public Result<User> update(User user);
+}
