be6c74f274e7a1cfef076647952ac4b68f79d82d
smallcjy <2628035541@qq.com>
2024-05-28 20:17:58 +0800

    实现用户模块的数据库接口实现

--- backend/src/main/java/com/example/backend/mapper/UserMapper.java
+++ backend/src/main/java/com/example/backend/mapper/UserMapper.java
@@ -6,4 +6,13 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper extends BaseMapper<User>
-{}
+{
+    //根据id返回用户信息
+    User findUserById(Integer id);
+    //根据name返回用户信息
+    User findUserByName(String name);
+    //注册用户
+    int registerUser(User user);
+    //修改用户信息
+    int editorUser(User user);
+}
