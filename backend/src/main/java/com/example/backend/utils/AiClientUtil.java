af49ed5fd6fb7c2b73b5b3cf40795aba2e5b6266
smallcjy <2628035541@qq.com>
2024-05-30 07:46:40 +0800

    编写ai工具层

---
+++ backend/src/main/java/com/example/backend/utils/AiClientUtil.java
@@ -0,0 +1,47 @@
+package com.example.backend.utils;
+
+import com.zhipu.oapi.ClientV4;
+import com.zhipu.oapi.Constants;
+import com.zhipu.oapi.service.v4.model.ChatMessage;
+import com.zhipu.oapi.service.v4.model.ChatMessageRole;
+import com.zhipu.oapi.service.v4.model.ChatCompletionRequest;
+import com.zhipu.oapi.service.v4.model.ModelApiResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class AiClientUtil {
+
+    private static ClientV4 client = null;
+
+    public AiClientUtil(){
+        client = new ClientV4.Builder("{8aba98f13a2765c1e94837f45ab134e8.RlGDHsYSpdsQgY5D}").build();
+    }
+
+    //提供同步调用服务，调用后可以一次性得到结果
+    public static void chatAiWithMessages(List<String> message){
+        //创建ChatMessages列表
+        List<ChatMessage> chatMessages = new ArrayList<>();
+        ChatMessage chatMessage = new ChatMessage(ChatMessageRole.USER.value(), message);
+        chatMessages.add(chatMessage);
+        String requestId = String.format("request-%d", System.currentTimeMillis());
+
+        //编写prompt请求
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
+                .model(Constants.ModelChatGLM4)
+                .stream(Boolean.FALSE)
+                .invokeMethod(Constants.invokeMethod)
+                .messages(chatMessages)
+                .requestId(requestId)
+                .build();
+
+        ModelApiResponse invokeModelApiResp = client.invokeModelApi(chatCompletionRequest);
+
+        try {
+            System.out.println("aimodel output: " + mapper);
+        }
+    }
+
+}
