b4e1ac9816344e14596c3dbe4d0f98621a779a3f
val213 <1042594899@qq.com>
2024-05-30 15:13:15 +0800

    feat: 新建attachment表和相应的初始文件

---
+++ backend/src/main/java/com/example/backend/service/AttachmentService.java
@@ -0,0 +1,7 @@
+package com.example.backend.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.backend.entity.Attachment;
+
+public interface AttachmentService extends IService<Attachment> {
+}
---
+++ backend/src/main/java/com/example/backend/service/impl/AttachmentServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.backend.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.backend.entity.Attachment;
+import com.example.backend.mapper.AttachmentMapper;
+import com.example.backend.service.AttachmentService;
+
+public class AttachmentServiceImpl extends ServiceImpl<AttachmentMapper, Attachment> implements AttachmentService {
+}
