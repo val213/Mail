b3bda30c2ff76deb7f3a0b7465542a5161a5ca30
val213 <1042594899@qq.com>
2024-06-11 12:04:13 +0800

    初步完成MailDetail接口，后续完善需要协调修改Mail中attachmentName、attachmentIds、url相关的字段和attachment表的内容

--- backend/src/main/java/com/example/backend/mapper/AttachmentMapper.java
+++ backend/src/main/java/com/example/backend/mapper/AttachmentMapper.java
@@ -3,6 +3,8 @@ package com.example.backend.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.backend.entity.Attachment;
 import com.example.backend.entity.Mail;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface AttachmentMapper extends BaseMapper<Attachment> {
 }
