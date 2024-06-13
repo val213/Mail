532027b1b0664fe879209868408b85a5cb2c022b
smallcjy <2628035541@qq.com>
2024-06-13 13:07:33 +0800

    feat：添加token请求拦截并认证token请求

---
+++ backend/src/main/java/com/example/backend/config/WebConfiguration.java
@@ -0,0 +1,36 @@
+package com.example.backend.config;
+
+import com.example.backend.filter.TokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Autowired
+    private TokenFilter tokenFilter;
+
+    public WebConfiguration(TokenFilter tokenFilter){
+        this.tokenFilter=tokenFilter;
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+
+        //排除对注册登录的请求拦截
+        List<String> excludePath = new ArrayList<>();
+        excludePath.add("/user/login");
+        excludePath.add("/user/register");
+        excludePath.add("/user/sendverifycode");
+
+        registry.addInterceptor(tokenFilter)
+                .addPathPatterns("/**")
+                .excludePathPatterns(excludePath);
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+}
